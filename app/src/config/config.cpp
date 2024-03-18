4a092b7ee93fcc52e3381ec281122fcaa629db8e
bl3utide <bl3utide@gmail.com>
2024-03-18 22:51:18 +0900

    impl import functions

--- app/src/config/config.cpp
+++ app/src/config/config.cpp
@@ -15,30 +15,26 @@ enum class Section : int
     _COUNT_,
 };
 
-enum class DeviceKey : int
+const std::string SECTIONS[static_cast<unsigned long long>(Section::_COUNT_)]
 {
-    InputDevice,
-    OutputDevice,
-    ToChannel,
-    ForceAdjustMidiCh,
-    _COUNT_,
+    "Device"
 };
 
 // TODO move to config/cv.hpp
 /*
-    Configuration Value Class
+    Configuration Section-Key-Value Class
 
     compatible types(expression in .ini)
     - bool          ("1" or "0")
     - int           ("1", "-10")
     - std::string   (same)
 */
-template <class T>
+template <typename T>
 class Cv
 {
 public:
-    Cv(const T& v_min, const T& v_max, const T& v_def)
-        : _min(v_min), _max(v_max), _def(v_def)
+    Cv(const Section section, const std::string& key, const T& v_min, const T& v_max, const T& v_def)
+        : section_name(SECTIONS[static_cast<unsigned long long>(section)]), key_name(key), _min(v_min), _max(v_max), _def(v_def)
     {
         if constexpr (std::is_integral<T>::value)
         {
@@ -87,6 +83,9 @@ public:
     int min() const noexcept { return _min; }
     void setDefault() noexcept { _config_value = _def; }
 
+    std::string section_name;
+    std::string key_name;
+
 private:
     T _config_value;
     T _min, _max, _def;
@@ -103,93 +102,76 @@ private:
     };
 };
 
-// TODO move to config/model.hpp
-struct InternalData
-{
-    Cv<std::string> in_dev_name { "", "", "" };         // InputDevice
-    Cv<std::string> out_dev_name{ "", "", "" };         // OuputDevice
-    Cv<int>         to_ch       { 1, 16, 1 };           // ToChannel
-    Cv<bool>        is_force_adj{ true, true, true };   // ForceAdjustMidiCh
+// TODO 3/18 今日やる！Cvの中に「どのセクションに属するか」を表現するためのセクション名の文字列を持たせる
+//      そうすると、下のようなSectionDeviceのように、「セクションごとのキーの集合」を束縛するための構造体が不要になる
+//      Sectionの名前は定数化し、この名前空間スコープ内で宣言しておく
+//      こうすることで、setValueやwrite系の関数の引数が1つで済むことになる
 
-    InternalData()
-    {}
-};
+Cv<std::string> in_dev_name { Section::Device,  "input_device",         "",     "",     "" };
+Cv<std::string> out_dev_name{ Section::Device,  "output_device",        "",     "",     "" };
+Cv<int>         to_ch       { Section::Device,  "to_channel",           1,      16,     1 };
+Cv<bool>        is_force_adj{ Section::Device,  "force_adjust_midi_ch", true,   true,   true };
+Cv<char>        dummy       { Section::Device,  "dummy",                0,     10,     0 };
 
 // private
-const char* _section_names[static_cast<int>(Section::_COUNT_)]
-{
-    "Device",
-};
-const char* _device_key_names[static_cast<int>(DeviceKey::_COUNT_)]
-{
-    "InputDevice",
-    "OutputDevice",
-    "ToChannel",
-    "ForceAdjustMidiCh",
-};
 mINI::INIStructure _is;
 std::string _file_name;
-InternalData _internal;
 bool _use_pretty_print = true;
 
-// TODO change func name to more meaningful name
-template<class T>
-void setValue(const char* section, const char* key, Cv<T>* cv)
-{
-    if (_is.get(section).has(key))
-    {
-        const std::string& target = _is.get(section).get(key);
+// TODO config/import.cpp
+template<typename T>
+void setValue(Cv<T>& cv, const std::string& val_str) {}
 
-        if constexpr (std::is_same_v<T, std::string>)
-        {
-            cv->set(target);
-        }
-        else if constexpr (std::is_same_v<T, int>)
-        {
-            if (std::atoi(target.c_str()))
-            {
-                cv->set(std::stoi(target));
-            }
-            else
-            {
-                cv->setDefault();
-            }
-        }
-        else if constexpr (std::is_same_v<T, bool>)
-        {
-            if (target == "1")
-            {
-                cv->set(true);
-            }
-            else
-            {
-                cv->set(false);
-            }
-        }
-        else
-        {
-            // unexpected type of value
-            throw std::exception(format("Failed to load config from ini because [%s]%s type is unexpected"));
-        }
+template <>
+void setValue<std::string>(Cv<std::string>& cv, const std::string& val_str)
+{
+    cv.set(val_str);
+}
+
+template <>
+void setValue<int>(Cv<int>& cv, const std::string& val_str)
+{
+    if (std::atoi(val_str.c_str()))
+        cv.set(std::stoi(val_str));
+    else
+        cv.setDefault();
+}
+
+template <>
+void setValue<bool>(Cv<bool>& cv, const std::string& val_str)
+{
+    if (val_str == "1")
+        cv.set(true);
+    else
+        cv.set(false);
+}
+
+template<typename T>
+void setToStructure(Cv<T>& cv)
+{
+    if (_is.get(cv.section_name).has(cv.key_name))
+    {
+        const std::string& src_val = _is.get(cv.section_name).get(cv.key_name);
+
+        setValue<T>(cv, src_val);
 #ifdef _DEBUG
-        LOGD << "[" << section << "]" << key << " is set to value: " << cv->cv();
+        LOGD << "[" << cv.section_name << "]" << cv.key_name << " is set to value: " << cv.cv();
 #endif
     }
     else
     {
-        cv->setDefault();
+        cv.setDefault();
 #ifdef _DEBUG
-        LOGD << "[" << section << "]" << key << " is set to default value: " << cv->cv();
+        LOGD << "[" << cv.section_name << "]" << cv.key_name << " is set to default value: " << cv.cv();
 #endif
     }
 }
+// config/import.cpp
 
 void initialize(const std::string& ini_file_name)
 {
     _file_name = ini_file_name;
     mINI::INIFile file = mINI::INIFile(_file_name);
-
-    _internal = InternalData();
 
     if (file.read(_is))
     {
@@ -197,12 +179,10 @@ void initialize(const std::string& ini_file_name)
         LOGD << "Load config from existing ini file";
 #endif
         // ini-file already exists
-
-        // [Device]
-        setValue<std::string>(_section_names[static_cast<int>(Section::Device)], _device_key_names[static_cast<int>(DeviceKey::InputDevice)], &_internal.in_dev_name);
-        setValue<std::string>(_section_names[static_cast<int>(Section::Device)], _device_key_names[static_cast<int>(DeviceKey::OutputDevice)], &_internal.out_dev_name);
-        setValue<int>(_section_names[static_cast<int>(Section::Device)], _device_key_names[static_cast<int>(DeviceKey::ToChannel)], &_internal.to_ch);
-        setValue<bool>(_section_names[static_cast<int>(Section::Device)], _device_key_names[static_cast<int>(DeviceKey::ForceAdjustMidiCh)], &_internal.is_force_adj);
+        setToStructure<std::string>(in_dev_name);
+        setToStructure<std::string>(out_dev_name);
+        setToStructure<int>(to_ch);
+        setToStructure<bool>(is_force_adj);
     }
 #ifdef _DEBUG
     else
@@ -210,17 +190,20 @@ void initialize(const std::string& ini_file_name)
         LOGD << "Ini file does not exists";
     }
 #endif
+
+    _is.clear();
 }
 
 void finalize() noexcept
 {
     mINI::INIFile file = mINI::INIFile(_file_name);
 
-    // [Device]
-    _is[_section_names[static_cast<int>(Section::Device)]][_device_key_names[static_cast<int>(DeviceKey::InputDevice)]] = _internal.in_dev_name.cv();
-    _is[_section_names[static_cast<int>(Section::Device)]][_device_key_names[static_cast<int>(DeviceKey::OutputDevice)]] = _internal.out_dev_name.cv();
-    _is[_section_names[static_cast<int>(Section::Device)]][_device_key_names[static_cast<int>(DeviceKey::ToChannel)]] = format("%d", _internal.to_ch.cv());
-    _is[_section_names[static_cast<int>(Section::Device)]][_device_key_names[static_cast<int>(DeviceKey::ForceAdjustMidiCh)]] = _internal.is_force_adj.cv() ? "1" : "0";
+    // TODO impl write function
+    _is[in_dev_name.section_name][in_dev_name.key_name] = in_dev_name.cv();
+    _is[out_dev_name.section_name][out_dev_name.key_name] = out_dev_name.cv();
+    _is[to_ch.section_name][to_ch.key_name] = format("%d", to_ch.cv());
+    _is[is_force_adj.section_name][is_force_adj.key_name] = is_force_adj.cv() ? "1" : "0";
+
     if (!file.write(_is, _use_pretty_print))
     {
 #ifdef _DEBUG
