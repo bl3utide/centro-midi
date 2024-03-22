a05cc0529cbe5c5bf26090dfafa1a25cbc8d60f2
bl3utide <bl3utide@gmail.com>
2024-03-22 21:02:50 +0900

    Writer::cvToStructure

--- app/src/config/writer.cpp
+++ app/src/config/writer.cpp
@@ -9,19 +9,19 @@ namespace Writer
 {
 
 template<>
-void cvToStructure<std::string>(Cv<std::string>& cv, mINI::INIStructure& is) noexcept
+void setToStructure<std::string>(Cv<std::string>& cv, mINI::INIStructure& is) noexcept
 {
     is[cv.section_name][cv.key_name] = cv.cv();
 }
 
 template<>
-void cvToStructure<int>(Cv<int>& cv, mINI::INIStructure& is) noexcept
+void setToStructure<int>(Cv<int>& cv, mINI::INIStructure& is) noexcept
 {
     is[cv.section_name][cv.key_name] = format("%d", cv.cv());
 }
 
 template<>
-void cvToStructure<bool>(Cv<bool>& cv, mINI::INIStructure& is) noexcept
+void setToStructure<bool>(Cv<bool>& cv, mINI::INIStructure& is) noexcept
 {
     is[cv.section_name][cv.key_name] = cv.cv() ? "1" : "0";
 }
